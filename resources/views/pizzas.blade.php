de80f37a22363d15c8624c9a05eb64f6822a8820
Perry Bates <perryboateng08@gmail.com>
2020-12-15 15:52:54 +0000

    my fourth commit

---
+++ resources/views/pizzas.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.layout')
+@section('content')
+<div class="flex-center position-ref full-height">
+    
+    <div class="content">
+        <div class="title m-b-md">
+        Pizzas List
+        </div>
+        
+        @foreach($pizzas as $pizza)
+         <div>
+            {{ $pizza['type'] }} - {{ $pizza['base'] }}
+         </div>
+        @endforeach
+    </div>
+</div>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
- 
-    
+Route::get('/pizzas','PizzaController@index');
+Route::get('/pizzas/{id}', 'PizzaController@show');
