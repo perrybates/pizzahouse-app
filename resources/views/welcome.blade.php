eb6d17ad2357a70f82dd60f894aea6f1b95723b1
Perry Bates <perryboateng08@gmail.com>
2020-12-15 12:06:22 +0000

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Pizzahouse
+                    PizzaHouse
                 </div>
 
                 <div class="links">
